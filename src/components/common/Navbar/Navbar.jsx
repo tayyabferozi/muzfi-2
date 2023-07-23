8d0d953e04e1cd9fec4dec445efedfccf2427732
sohanemon <m.sohanemon@gmail.com>
2023-07-23 11:38:41 +0600

    fix: navbar

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,66 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import GradientBtn from "components/buttons/GradientBtn";
-import Search from "components/common/Search";
+import GradientBtn from 'components/buttons/GradientBtn';
+import Search from 'components/common/Search';
 
 const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   return (
-    <div className="fixed top-0 left-0 w-screen bg-primary h-[70px] flex justify-between items-center py-4 sm:px-[24px] px-[12px] z-20">
-      <div className="flex-grow xl:w-1/3 h-5 flex items-center gap-3">
-        <div
-          className="xl:hidden flex flex-col gap-1"
-          onClick={() => setIsSidebarOpen((prevState) => !prevState)}
-        >
-          <div className="bg-white w-5 h-[2px]"></div>
-          <div className="bg-white w-5 h-[2px]"></div>
-          <div className="bg-white w-5 h-[2px]"></div>
-        </div>
+    <nav className='fixed inset-x-0 top-0 z-50 shadow-lg bg-primary'>
+      <div className='   container   h-[70px] flex justify-between items-center py-4 sm:px-[24px] px-[12px] z-20'>
+        <div className='flex items-center flex-grow h-5 gap-3 xl:w-1/3 '>
+          <div
+            className='flex flex-col gap-1 xl:hidden'
+            onClick={() => setIsSidebarOpen((prevState) => !prevState)}
+          >
+            <div className='bg-white w-5 h-[2px]'></div>
+            <div className='bg-white w-5 h-[2px]'></div>
+            <div className='bg-white w-5 h-[2px]'></div>
+          </div>
 
-        <Link className="hidden min-[1185px]:block ml-14" to="/">
-          <img
-            className="mx-auto h-12"
-            src="/assets/imgs/common/logo.png"
-            alt="logo"
-          />
-        </Link>
-        <Link className="hidden min-[1185px]:hidden lg:block ml-6" to="/">
-          <img
-            className="mx-auto h-12"
-            src="/assets/imgs/common/logo-1.png"
-            alt="logo"
-          />
-        </Link>
-      </div>
-      <div className="flex-shrink-0 flex-grow  lg:flex justify-center hidden">
-        <Search />
-      </div>
-      <div className="xl:w-1/3 flex-shrink-0 flex justify-end items-center ">
-        <div className="flex items-center sm:gap-[22px] gap-3">
-          <Link className="hover:scale-105 transition-transform" to="/">
-            <img src="/assets/vectors/navbar/earth.svg" alt="earth" />
+          <Link className='hidden min-[1185px]:block ml-14' to='/'>
+            <img
+              className='h-12 mx-auto'
+              src='/assets/imgs/common/logo.png'
+              alt='logo'
+            />
           </Link>
-          {/* <Link className="hover:scale-105 transition-transform" to="/">
+          <Link className='hidden min-[1185px]:hidden lg:block ml-6' to='/'>
+            <img
+              className='h-12 mx-auto'
+              src='/assets/imgs/common/logo-1.png'
+              alt='logo'
+            />
+          </Link>
+        </div>
+        <div className='justify-center flex-grow flex-shrink-0 hidden lg:flex'>
+          <Search />
+        </div>
+        <div className='flex items-center justify-end flex-shrink-0 xl:w-1/3 '>
+          <div className='flex items-center sm:gap-[22px] gap-3'>
+            <Link className='transition-transform hover:scale-105' to='/'>
+              <img src='/assets/vectors/navbar/earth.svg' alt='earth' />
+            </Link>
+            {/* <Link className="transition-transform hover:scale-105" to="/">
             <img
               src="/assets/vectors/navbar/notification.svg"
               alt="notification"
             />
           </Link> */}
-          <Link className="hover:scale-105 transition-transform" to="/">
-            <img className="h-6" src="/assets/imgs/home/cart.png" alt="cart" />
-          </Link>
+            <Link className='transition-transform hover:scale-105' to='/'>
+              <img
+                className='h-6'
+                src='/assets/imgs/home/cart.png'
+                alt='cart'
+              />
+            </Link>
 
-          <div className="w-[1px] h-[22px] bg-white bg-opacity-50 sm:block hidden"></div>
-          <Link
-            className="text-white font-semibold text-base hover:text-opacity-75 transition-colors"
-            to="/"
-          >
-            Login
-          </Link>
+            <div className='w-[1px] h-[22px] bg-white bg-opacity-50 sm:block hidden'></div>
+            <Link
+              className='text-base font-semibold text-white transition-colors hover:text-opacity-75'
+              to='/'
+            >
+              Login
+            </Link>
 
-          <GradientBtn label="Register" />
+            <GradientBtn label='Register' />
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,32 +1,31 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import Sidebar from "components/common/Sidebar";
-import Navbar from "components/common/Navbar";
-import Explore from "components/common/Explore";
+import Sidebar from 'components/common/Sidebar';
+import Navbar from 'components/common/Navbar';
+import Explore from 'components/common/Explore';
 
 const MainLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className="overflow-hidden">
+    <div className='overflow-hidden '>
       <Navbar
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
 
-      <div className="bg-accentGray-500 w-screen overflow-hidden flex lg:flex-row flex-col p-6 pt-28 ">
+      <div className='container flex flex-col w-screen p-6 overflow-hidden bg-accentGray-500 lg:flex-row pt-28 '>
         <Sidebar
-
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
 
-        <div className="flex-grow xl:pl-6 pl-0 pb-0 px-[10px] lg:mr-[rem] xl:mr-[1rem] 2xl:mr-[-5rem]">
+        <div className='flex-grow xl:pl-6 pl-0 pb-0 px-[10px] lg:mr-[rem] xl:mr-[1rem] 2xl:mr-[-5rem]'>
           <Outlet />
         </div>
 
-        <div className="2xl:w-[330px] xl:w-[300px] lg:w-[320px]  mx-auto lg:mr-[3rem] xl:mr-[3rem]">
+        <div className='2xl:w-[330px] xl:w-[300px] lg:w-[320px]  mx-auto lg:mr-[3rem] xl:mr-[3rem]'>
           <Explore />
         </div>
       </div>
