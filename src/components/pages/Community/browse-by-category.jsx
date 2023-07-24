b3a0c9aecbe2c110b9e58b2b77c4a68f3e3b356d
sohanemon <m.sohanemon@gmail.com>
2023-07-24 10:16:30 +0600

    fin: category

--- src/components/pages/Community/browse-by-category.jsx
+++ src/components/pages/Community/browse-by-category.jsx
@@ -4,84 +4,10 @@ import { Navigation } from 'swiper';
 import Card from 'components/common/Card';
 import useSwiperRef from 'hooks/useSwiperRef';
 
-const categories = [
-  {
-    img: '/assets/imgs/home/musicians.png',
-    label: 'General',
-  },
-  {
-    img: '/assets/imgs/home/genre.png',
-    label: "Genre's",
-  },
-  {
-    img: '/assets/imgs/home/keys.png',
-    label: 'Keys/Synths',
-  },
-  {
-    img: '/assets/imgs/home/drums.png',
-    label: 'Drums',
-  },
-  {
-    img: '/assets/imgs/home/guitar.png',
-    label: 'Guitar',
-  },
-  {
-    img: '/assets/imgs/home/bass.png',
-    label: 'Bass',
-  },
-  {
-    img: '/assets/imgs/home/pedalamps.png',
-    label: 'Pedals',
-  },
-  {
-    img: '/assets/imgs/home/gigs.png',
-    label: 'Gigging',
-  },
-  {
-    img: '/assets/imgs/home/live.png',
-    label: 'Live Sound',
-  },
-  {
-    img: '/assets/imgs/home/horns.png',
-    label: 'Horns',
-  },
-  {
-    img: '/assets/imgs/home/recording.png',
-    label: 'Studio',
-  },
-  {
-    img: '/assets/imgs/home/prod.png',
-    label: 'Producers',
-  },
-  {
-    img: '/assets/imgs/home/dj.png',
-    label: 'DJ / Audio',
-  },
-  {
-    img: '/assets/imgs/home/amplifier.png',
-    label: 'Amps',
-  },
-  {
-    img: '/assets/imgs/home/vintage.png',
-    label: 'Vintage/Rare',
-  },
-  {
-    img: '/assets/imgs/home/slider-img-5.png',
-    label: 'Vocals',
-  },
-  {
-    img: '/assets/imgs/home/mixing.png',
-    label: 'Mixing',
-  },
-  {
-    img: '/assets/imgs/home/workers.png',
-    label: 'Luthiers',
-  },
-  {
-    img: '/assets/imgs/home/wind.png',
-    label: 'Wind',
-  },
-];
+const category = {
+  img: '/assets/imgs/home/user-img-9.png',
+  label: 'General',
+};
 
 const BrowseCategories = () => {
   const [nextEl, nextRef] = useSwiperRef();
@@ -130,7 +56,7 @@ const BrowseCategories = () => {
                   },
                 }}
               >
-                {categories.map((el, idx) => {
+                {Array.from(Array(20)).map((el, idx) => {
                   return (
                     <SwiperSlide key={'home-category-slider-item' + idx}>
                       <div className='flex flex-col items-center'>
@@ -142,12 +68,12 @@ const BrowseCategories = () => {
                           />
                           <img
                             className='rounded-[50%] w-[48px] h-[48px] absolute left-2/4 -translate-x-2/4 top-2/4 -translate-y-2/4'
-                            src={el.img}
+                            src={category.img}
                             alt='slider'
                           />
                         </div>
                         <div className='mt-1 text-center font-medium text-[10px]'>
-                          {el.label}
+                          {category.label}
                         </div>
                       </div>
                     </SwiperSlide>
