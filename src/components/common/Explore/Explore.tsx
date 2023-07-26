8c49b04fbdab81c623c3f4a9ff4a0b4d227d90fa
sohanemon <m.sohanemon@gmail.com>
2023-07-26 16:40:33 +0600

    feat: user profile

--- src/components/common/Explore/Explore.tsx
+++ src/components/common/Explore/Explore.tsx
@@ -7,6 +7,8 @@ import { useLocation } from 'react-router-dom';
 import { Ruler } from '../ruler';
 import ExploreMenu from '../../pages/For-Sale/explore-menu';
 import Card from '../Card';
+import Profile from '../../pages/Create-Post/profile';
+import { useMemo } from 'react';
 
 const links = [
   'About Us',
@@ -19,58 +21,53 @@ const links = [
 
 const Explore = () => {
   const { pathname } = useLocation();
-  return (
-    <Card className='rounded-b-none lg:h-full lg:mt-0'>
-      {pathname === '/for-sale' ? (
-        <ExploreMenu />
-      ) : (
-        <>
-          <TopGearRooms />
-        </>
-      )}
-
-      <Ruler />
-
-      <SubCommunities />
-
-      <div className='my-6'>
-        <hr />
-      </div>
-
-      <Reviews2 />
-
-      <img className='w-full mt-4' src='/assets/imgs/home/ad-3.png' alt='ad' />
-
-      <Ruler />
-
-      <Reviews />
-
-      <Ruler />
-
-      <img className='mx-auto' src='/assets/imgs/home/users.png' alt='users' />
-      <div className='mt-[18px] flex items-center justify-center gap-2'>
-        <div className='text-[26px] text-[#1D1929] font-bold'>184.3K</div>
-        <div className='font-medium text-accentGray-700'>Followers</div>
-      </div>
-      <div className='font-medium text-center'>Active now on your profile</div>
-
-      <Ruler />
-
-      <div className='flex flex-wrap justify-center gap-6'>
-        {links.map((el, idx) => {
-          return (
-            <Link
-              className='text-accentGray-700'
-              to='/'
-              key={'footer-link' + idx}
-            >
-              {el}
-            </Link>
-          );
-        })}
-      </div>
-    </Card>
-  );
+  const content = useMemo(() => {
+    switch (true) {
+      case pathname.includes('for-sale'):
+        return <ExploreMenu />;
+      case pathname.includes('gearroom'):
+        return <Profile />;
+      default:
+        return extra;
+    }
+  }, [pathname]);
+  const Comp = pathname.includes('gearroom') ? 'div' : Card;
+  return <Comp className='rounded-b-none lg:h-full lg:mt-0'>{content}</Comp>;
 };
 
 export default Explore;
+
+const extra = (
+  <>
+    {' '}
+    <TopGearRooms />
+    <Ruler />
+    <SubCommunities />
+    <Ruler />
+    <Reviews2 />
+    <img className='w-full mt-4' src='/assets/imgs/home/ad-3.png' alt='ad' />
+    <Ruler />
+    <Reviews />
+    <Ruler />
+    <img className='mx-auto' src='/assets/imgs/home/users.png' alt='users' />
+    <div className='mt-[18px] flex items-center justify-center gap-2'>
+      <div className='text-[26px] text-[#1D1929] font-bold'>184.3K</div>
+      <div className='font-medium text-accentGray-700'>Followers</div>
+    </div>
+    <div className='font-medium text-center'>Active now on your profile</div>
+    <Ruler />
+    <div className='flex flex-wrap justify-center gap-6'>
+      {links.map((el, idx) => {
+        return (
+          <Link
+            className='text-accentGray-700'
+            to='/'
+            key={'footer-link' + idx}
+          >
+            {el}
+          </Link>
+        );
+      })}
+    </div>
+  </>
+);
