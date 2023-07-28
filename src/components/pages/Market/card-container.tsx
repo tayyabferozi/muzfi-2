4e7b7c3047d185b870ee56063035a795a454ea02
sohanemon <m.sohanemon@gmail.com>
2023-07-28 21:51:14 +0600

    feat: card

---
+++ src/components/pages/Market/card-container.tsx
@@ -0,0 +1,27 @@
+export default function CardContainer() {
+  return (
+    <section>
+      <p className='text-[22px] font-semibold mt-6'>Shop Top Brands</p>
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {Array.from(Array(12)).map((_) => (
+          <Card key={_} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+const Card = () => {
+  return (
+    <div className='bg-white border rounded-lg border-slate-400/20 p-1.5 '>
+      <div className='h-36 w-full bg-neutral-100 rounded-lg border backdrop-blur-[20px]'>
+        <img
+          className='object-cover object-bottom w-full h-full pb-3'
+          src='https://s3-alpha-sig.figma.com/img/4e69/4e4c/7f055d65e3c36e6085b1c5a963281e68?Expires=1691366400&Signature=WiChpkhbWXc0aSGS3aGXnPt1S17xXIua-HiI1Tybs1-bpslmn51Ztag8crR0QOUUFH9mvrmabZyAYFoX65rYG1bQCAn-2QbsCB5K704PZ7rSCdWojZzie~hRTw2b3FzFRp2r30ZiynTxrLTpUoiAkbO1teM8CHBhJrqux-Ye6nyjeDHkcc~PiaMt8LDL0VihVbClux6YnkFl52xfvnxVoje~a432CFrFKnduxZwoehZzFIzI-V8ZlSKWBf4deYmno4mC0TUcYwP7vLtNwb6jQKTJcVGxFJQdLl3XL8SinjZgXKTvooE6NDGI-sIw0Jc~RsPsEDvhGFxUZ4Rg6cVVXA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+          alt=''
+        />
+      </div>
+      <p className='text-xs font-semibold text-center pt-1.5 '>Fender</p>
+    </div>
+  );
+};
--- src/components/pages/Market/for-sale-topbar.tsx
+++ src/components/pages/Market/for-sale-topbar.tsx
@@ -1,13 +1,38 @@
 import { Icon } from '@iconify/react';
+import Select from '../../common/select';
+import { Input } from '../../common/input';
+import CardContainer from './card-container';
 
 export default function ForSaleTopBar() {
   return (
     <section>
-      <div className='p-2 bg-white border rounded-lg border-slate-400/20'>
-        <button className='border rounded-md h-11 bg-zinc-100 border-slate-400/20'>
-          <Icon icon={'ic:round-menu'} />
+      <div className='flex gap-4 p-2 bg-white border rounded-lg border-slate-400/20'>
+        <button className='flex items-center gap-1 p-3 border rounded-md h-11 bg-zinc-100 border-slate-400/20'>
+          <Icon icon={'ic:round-menu'} className='text-lg' />
+          <p className='text-sm font-medium'>Categories</p>
         </button>
+        <div className='text-sm font-semibold text-zinc-600 [&>button]:px-3'>
+          <button className='text-white rounded-md h-11 bg-gradient-to-l from-red-500 to-yellow-500'>
+            Shop
+          </button>
+          <button>About</button>
+        </div>
+        <Select
+          options={options}
+          placeholder='Sort by: Most Recent First'
+          className='w-full bg-zinc-100'
+        />
+        <div className='flex items-center border rounded-md bg-zinc-100 p-2.5 gap-3  border-slate-400/20'>
+          <Icon icon={'iconamoon:search'} />
+          <input
+            placeholder='Search all 75 items'
+            className='w-full bg-transparent'
+          />
+        </div>
       </div>
+      <CardContainer />
     </section>
   );
 }
+
+const options = [{ label: 'hello' }, { label: 'world' }];
