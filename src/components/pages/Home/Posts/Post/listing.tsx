406c3c7b43955ab9e36b3506c07838cf6be7d5d5
sohanemon <m.sohanemon@gmail.com>
2023-08-02 22:49:40 +0600

    chore: listing

--- src/components/pages/Home/Posts/Post/listing.tsx
+++ src/components/pages/Home/Posts/Post/listing.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import GradientBtn from '../../../../buttons/GradientBtn';
 import Card from '../../../../common/Card';
 import './Post.css';
 import { Author } from './author';
@@ -24,9 +26,9 @@ const Listing = ({ el, idx }: { el: any; idx: number }) => {
         {data.map((_) => (
           <div
             key={_.text}
-            className='p-0.5 rounded-md bg-gradient-to-r w-full h-fit from-amber-500 to-red-600'
+            className='w-full p-px rounded-md bg-gradient-to-r from-amber-500 to-red-600'
           >
-            <div className='flex bg-slate-100 gap-2.5 rounded px-2.5 py-3'>
+            <div className='flex bg-slate-100 gap-2.5 h-full items-center rounded-[5px] px-2.5 py-3'>
               <div className='p-1 rounded-full bg-amber-500'>
                 <img src={_.icon} alt='' />
               </div>
@@ -39,6 +41,30 @@ const Listing = ({ el, idx }: { el: any; idx: number }) => {
             </div>
           </div>
         ))}
+        <div className='flex flex-col items-center justify-between w-full gap-2'>
+          <div className='flex gap-2'>
+            <button
+              className={clsx(
+                'border border-solid border-accentYellow-300 transition-transform hover:scale-105 bg-accentGray-900 rounded-lg w-6 h-6 inline-flex justify-center items-center'
+              )}
+            >
+              <img
+                className='w-5 h-5'
+                src='/assets/vectors/common/heart.svg'
+                alt='heart'
+              />
+            </button>
+
+            <button className='h-6 transition-transform hover:scale-105 bg-accentGray-900 rounded-lg border border-solid border-accentYellow-300 text-[10px] font-medium px-4'>
+              Make an offer
+            </button>
+          </div>
+
+          <button className='h-6 commit-btn w-full transition-transform hover:scale-105 bg-black rounded-lg text-[10px] font-medium text-white flex items-center justify-center py-[10px] px-7 gap-5'>
+            <img src='/assets/vectors/common/cart.svg' alt='cart' />
+            Commit
+          </button>
+        </div>
       </div>
       {el.prodDescription && (
         <div className='mt-4 text-xs font-medium text-accentGray-800'>
