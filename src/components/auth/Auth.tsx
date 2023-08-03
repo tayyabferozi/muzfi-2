7eed81bc6270d002f6913eb1589180f1dbc30d58
sohanemon <m.sohanemon@gmail.com>
2023-08-03 09:18:05 +0600

    feat: separate scrolling

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { authClose } from "../../redux/features/auth/authPopupSlice";
-import { handlePrev } from "../../redux/features/auth/authTypeSlice";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { inputAssets } from "./inputs";
-import LoginForm from "./LoginForm";
-import RegisterForm from "./RegisterForm";
+import React, { useState } from 'react';
+import { authClose } from '../../redux/features/auth/authPopupSlice';
+import { handlePrev } from '../../redux/features/auth/authTypeSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { inputAssets } from './inputs';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 
 interface ModalProps {
-    isModalActive: boolean;
-    type: String
+  isModalActive: boolean;
+  type: String;
 }
 
 const Auth = ({ isModalActive, type }: ModalProps) => {
@@ -18,9 +18,9 @@ const Auth = ({ isModalActive, type }: ModalProps) => {
     if (hidden.screen === true) {
       return (
         <img
-          className="h-6 "
-          src="/assets/imgs/home/back.png"
-          alt="back"
+          className='h-6 '
+          src='/assets/imgs/home/back.png'
+          alt='back'
           onClick={() => {
             dispatch(handlePrev());
           }}
@@ -30,33 +30,32 @@ const Auth = ({ isModalActive, type }: ModalProps) => {
       return null;
     }
   };
-    if (!isModalActive) return null;
-    
-    const findType = () => {
-        if (type === "signin") {
-            return <LoginForm inputAssets={inputAssets} />
-        } else {
-            return <RegisterForm/>
-        }
+  if (!isModalActive) return null;
 
+  const findType = () => {
+    if (type === 'signin') {
+      return <LoginForm inputAssets={inputAssets} />;
+    } else {
+      return <RegisterForm />;
     }
+  };
 
   return (
-    <div className="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-25 backdrop-blur-sm grid place-items-center">
-      <div className="tablet:w-[350px] w-[450px] flex flex-col relative">
+    <div className='fixed inset-0 z-50 grid overflow-x-hidden overflow-y-auto bg-black bg-opacity-25 outline-none focus:outline-none backdrop-blur-sm place-items-center'>
+      <div className='tablet:w-[350px] w-[450px] flex flex-col relative'>
         {/*  */}
-        <div className="h-fit w-100px bg-white pt-2 p-5 rounded-xl m-auto">
-          <div className="flex ">
+        <div className='p-5 pt-2 m-auto bg-white h-fit w-100px rounded-xl'>
+          <div className='flex '>
             {data()}
             <img
-              className="h-8 m-auto"
-              src="/assets/imgs/common/logo-2.png"
-              alt="logo"
+              className='h-8 m-auto'
+              src='/assets/imgs/common/logo-2.png'
+              alt='logo'
             />
             <img
-              className="h-6 "
-              src="/assets/imgs/common/cancel.png"
-              alt="cancel"
+              className='h-6 '
+              src='/assets/imgs/common/cancel.png'
+              alt='cancel'
               onClick={() => {
                 dispatch(authClose());
               }}
--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = ({
   return (
     <div
       className={clsx(
-        'fixed bg-accentGray-500 top-[70px] xl:static xl:h-[unset] h-[calc(100vh-70px)] z-10 min-w-[210px] lg:min-w-[280px] overflow-auto flex-shrink-0 ',
+        'fixed lg:sticky lg:top-0 lg:self-start lg:h-full scrollbar-hide bg-accentGray-500 top-[70px] xl:static xl:h-[unset] h-[calc(100vh-70px)] z-10 min-w-[210px] lg:min-w-[280px] overflow-auto flex-shrink-0 ',
         isSidebarOpen ? 'left-0' : '-left-[300px]'
       )}
       id='main-layout-sidebar'
