4d8ae8b886529d00a0b17d2d2341aa684c847b8c
sohanemon <m.sohanemon@gmail.com>
2023-08-03 08:37:46 +0600

    fix: off spacing

--- src/components/pages/Pagelayout.tsx
+++ src/components/pages/Pagelayout.tsx
@@ -1,6 +1,3 @@
-
-
-
 const Pagelayout = () => {
   /// ADD Logic for 2 different Homepages Based on User being logged in
 
@@ -9,12 +6,11 @@ const Pagelayout = () => {
   /// ADD Member SignedIN Homepage(1-2. Homepage) with logic once user log's in
 
   return (
-    <div className="gap-6 lg:flex">
-      <div className="flex-grow min-[1700px]:max-w-[850px] 2xl:max-w-[700px] min-[1350px]:max-w-[675px] xl:max-w-[585px] min-[1150px]:max-w-[675px] lg:max-w-[600px] sm:w-[calc(100vw-48px)] w-[calc(100vw-48px)]  mx-auto">
+    <div className='gap-6 lg:flex'>
+      <div className='w-full  mx-auto'>
         {/* <div className='mb-5 lg:hidden'>
           <Search />
         </div> */}
-     
       </div>
     </div>
   );
