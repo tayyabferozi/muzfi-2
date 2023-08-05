7a058fa6bfa2807d542f70d10b2a2b1acbae711e
sohanemon <m.sohanemon@gmail.com>
2023-08-06 00:06:50 +0600

    fix: listing

--- src/components/pages/My-Gear-Room/index.tsx
+++ src/components/pages/My-Gear-Room/index.tsx
@@ -1,13 +1,11 @@
-import Carousel from './carousel';
-import Details from './details';
+import ComingSoon from '../../common/Error/ComingSoon';
 
 export default function MyGearRoom() {
   // This page will be 10. User Gear Page !! Not 9. Threads Etc
 
   return (
-    <section className='grid-cols-2 gap-6 lg:grid'>
-      <Carousel />
-      <Details />
+    <section>
+      <ComingSoon />
     </section>
   );
 }
