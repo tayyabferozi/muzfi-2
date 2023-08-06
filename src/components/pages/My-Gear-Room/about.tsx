42ba7bd169bd1b4ef71cabd8c457ff46c79edc53
sohanemon <m.sohanemon@gmail.com>
2023-08-06 13:03:25 +0600

    feat: gradientbtn

--- src/components/pages/My-Gear-Room/about.tsx
+++ src/components/pages/My-Gear-Room/about.tsx
@@ -1,19 +1,21 @@
+import { Icon } from '@iconify/react';
 import { useState } from 'react';
+import GradientBtn from '../../buttons/GradientBtn/GradientBtn';
 
 export default function About() {
-  const [isExpandedState, setIsExpandedState] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <section className='mt-4'>
       <p className='text-lg font-semibold text-black'>About us</p>
       <div className='mt-3 text-base font-normal'>
-        {isExpandedState ? (
+        {isExpanded ? (
           <>
             {text}
             {text.length > 220 && (
               <span
                 className='cursor-pointer text-accentYellow-500'
-                onClick={() => setIsExpandedState(false)}
+                onClick={() => setIsExpanded(false)}
               >
                 {' '}
                 Read less
@@ -28,7 +30,7 @@ export default function About() {
                 ...{' '}
                 <span
                   className='cursor-pointer text-accentYellow-500'
-                  onClick={() => setIsExpandedState(true)}
+                  onClick={() => setIsExpanded(true)}
                 >
                   Read more
                 </span>
@@ -37,9 +39,42 @@ export default function About() {
           </>
         )}
       </div>
+      <div className='flex justify-center gap-5 my-3'>
+        {action.map((_) => (
+          <GradientBtn
+            label={
+              <span className='flex items-center gap-1.5'>
+                <Icon icon={_.icon} className='text-xl' />
+                <p className='font-bold '>{_.label}</p>
+              </span>
+            }
+            key={_.label}
+            className='flex '
+          />
+        ))}
+      </div>
     </section>
   );
 }
 
 const text =
   "Hi folks I'm Matthew,  matt just turned 18 started when I was 16- 2020 I'm a guitar, banjo, steel guitar and mandolin player small collector instrument enthusiast mostly. Hi folks I'm Matthew, matt just turned 18 started when I was 16- 2020 I'm a guitar, banjo, steel guitar and mandolin player small collector instrument enthusiast mostly. Hi folks I'm Matthew, matt just turned 18 started when I was 16- 2020 I'm a guitar, banjo, steel guitar and mandolin player small collector instrument enthusiast mostly.";
+
+const action = [
+  {
+    icon: 'ri:star-line',
+    label: 'Reviews',
+  },
+  {
+    icon: 'mdi:cart-percent',
+    label: 'For Sale',
+  },
+  {
+    icon: 'tabler:needle-thread',
+    label: 'Threads',
+  },
+  {
+    icon: 'mdi:comments-outline',
+    label: 'Comments',
+  },
+];
