5ad5b255f4d27c4bcb5ff064b511de759c3e994c
sohanemon <m.sohanemon@gmail.com>
2023-08-11 04:01:54 +0600

    feat: promoted post

--- src/components/pages/Single-Page/Topic/author.tsx
+++ src/components/pages/Single-Page/Topic/author.tsx
@@ -2,7 +2,9 @@ import { HTMLAttributes } from 'react';
 import { cn } from '../../../../helpers/utils';
 import { Icon } from '@iconify/react';
 
-interface AuthorProps extends HTMLAttributes<HTMLDivElement> {}
+interface AuthorProps extends HTMLAttributes<HTMLDivElement> {
+  isPromoted?: boolean;
+}
 
 export default function Author({ className, ...props }: AuthorProps) {
   return (
@@ -27,16 +29,22 @@ export default function Author({ className, ...props }: AuthorProps) {
         </div>
         <div className='flex items-center gap-1 text-xs font-medium text-neutral-400'>
           <Icon icon='lucide:dot' className='max-sm:hidden' />
-          <p>Posted by u/BgRobFox12</p>
-          <Icon icon='lucide:dot' />
-          <p>7 hours ago</p>
+          <p>{props.isPromoted ? 'Promoted' : 'Posted by u/BgRobFox12'}</p>
+          {props.isPromoted ? null : (
+            <>
+              <Icon icon='lucide:dot' />
+              <p>7 hours ago</p>
+            </>
+          )}
         </div>
       </div>
-      <Icon
-        role='button'
-        icon={'iconamoon:notification'}
-        className='text-lg text-neutral-400 '
-      />
+      {props.isPromoted ? null : (
+        <Icon
+          role='button'
+          icon={'iconamoon:notification'}
+          className='text-lg text-neutral-400 '
+        />
+      )}
     </section>
   );
 }
--- src/components/pages/Single-Page/Topic/index.tsx
+++ src/components/pages/Single-Page/Topic/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import CommentSection from '../Listing/comment-section';
 import SimilarProduct from '../Listing/similar-product';
 import TopicCard from './topic-card';
+import PromotedPost from './promoted-post';
 
 const TopicPage = () => {
   // This will be 9 Thread Single Page: topics page
   return (
     <section>
       <TopicCard />
+      <PromotedPost />
       <CommentSection />
       <SimilarProduct />
     </section>
